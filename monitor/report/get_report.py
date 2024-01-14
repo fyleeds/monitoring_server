b7c5cc5927801e6df25bc361a37aa174aeaeb0b5
cpenot <clem.cmp@hotmail.com>
2024-01-14 14:21:11 +0000

    refactore get report

--- monitor/report/get_report.py
+++ monitor/report/get_report.py
@@ -2,7 +2,7 @@ import sys
 import os
 from datetime import datetime,timedelta
 import pytz
-import json
+
 
 sys.path.append(os.path.abspath('../log'))
 from logger_config import setup_logger
@@ -11,7 +11,7 @@ logger = setup_logger("get_report_logger")
 
 # Ajouter le chemin vers log_folder à sys.path
 sys.path.append(os.path.abspath('../storage'))
-from get_storage import getListStorage
+from get_storage import getListStorage,getJsonFile
 
 base_path = "../../var/monit"
 path_reports = base_path + "/reports/"
@@ -20,32 +20,11 @@ path_reports_average = base_path + "/reports_average/"
 tz= pytz.timezone('Europe/Paris')
 date =  datetime.now(tz)
 
-def getListReportsLastHours(hours):
-    list_reports = []
-    date_wanted = date - timedelta(hours=hours)
-    for report_name in getListStorage(path_reports):
-        if getDateTimeReport(report_name) > date_wanted:
-            list_reports.append(report_name)
-    return list_reports
-
-def getDateTimeReport(report_name):
-    date_report = tz.localize(datetime.strptime(" ".join(report_name.split(".")[0].split("_")[len(report_name.split(".")[0].split("_"))-2:]), "%d-%m-%Y %H-%M-%S"))
-    return date_report
-
 def getReportRamInfos(report_name):
-    report = getReportAsDict(report_name)
-    return report["ram"].items()
+    return getJsonFile(path_reports+report_name)["ram"].items()
 
 def getReportCpuInfos(report_name):
-    report = getReportAsDict(report_name)
-    return report["cpu"].items()
-
-def getReportAsStr(report_name):
-    with open(path_reports+"/"+report_name) as report:
-        return report.read()
-    
-def getReportAsDict(report_name):
-    return json.loads(getReportAsStr(report_name))
+    return getJsonFile(path_reports+report_name)["cpu"].items()
 
 def getLastReportName():
     date2 = datetime(1970,1,1,0,0,0,0,tzinfo=tz)
@@ -63,6 +42,17 @@ def getLastReportAverageName():
             most_recent_report = report_name
     return most_recent_report
 
+def getDateTimeReport(report_name):
+    date_report = tz.localize(datetime.strptime(" ".join(report_name.split(".")[0].split("_")[len(report_name.split(".")[0].split("_"))-2:]), "%d-%m-%Y %H-%M-%S"))
+    return date_report
+
+def getListReportsLastHours(hours):
+    list_reports = []
+    date_wanted = date - timedelta(hours=hours)
+    for report_name in getListStorage(path_reports):
+        if getDateTimeReport(report_name) > date_wanted:
+            list_reports.append(report_name)
+    return list_reports
 
 # def main():
 
--- monitor/storage/get_storage.py
+++ monitor/storage/get_storage.py
@@ -1,4 +1,13 @@
 import os
+import json
 
 def getListStorage(dir):
     return [file.name for file in os.scandir(dir)]
+
+def readFile(path):
+    with open(path) as report:
+        return report.read()
+    
+def getJsonFile(path):
+    with open(path) as report:
+        return json.loads(report.read())
